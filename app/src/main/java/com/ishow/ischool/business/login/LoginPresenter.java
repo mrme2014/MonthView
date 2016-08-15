6c6d213aea7fce5d20a1b1e5b2e7b50c3923112a
wqf <wuqifeng@qq.com>
2016-08-15 23:37:40 +0800

    添加仿美团选择城市列表

--- app/src/main/java/com/ishow/ischool/business/login/LoginPresenter.java
+++ app/src/main/java/com/ishow/ischool/business/login/LoginPresenter.java
@@ -15,7 +15,7 @@ public class LoginPresenter extends LoginContract.Presenter {
     @Override
     public void login(String name, String pass) {
         mModel.login(name, pass)
-                .subscribe(new ApiObserver<User>(User.class) {
+                .subscribe(new ApiObserver<User>() {
                     @Override
                     public void onSuccess(User user) {
                         if (user != null && user.getToken() != null) {

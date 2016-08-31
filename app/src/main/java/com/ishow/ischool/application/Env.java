77f514af13a07d14c316846067def3980344e2ab
abel wang <xbinwang@qq.com>
2016-08-31 17:36:37 +0800

    修改编辑脚本

--- app/src/main/java/com/ishow/ischool/application/Env.java
+++ app/src/main/java/com/ishow/ischool/application/Env.java
@@ -1,11 +1,12 @@
 package com.ishow.ischool.application;
 
+import com.ishow.ischool.BuildConfig;
+
 /**
  * Created by abel on 16/8/8.
  */
 public class Env {
-
     public static final String SITE_URL_RELEASE = "http://crmcore.ishowedu.com/";
     public static final String SITE_URL_DEV = "http://crmcore-dev.ishowedu.com/";
-    public static final String SITE_URL = SITE_URL_DEV;
+    public static final String SITE_URL = BuildConfig.release ? SITE_URL_RELEASE : SITE_URL_DEV;
 }

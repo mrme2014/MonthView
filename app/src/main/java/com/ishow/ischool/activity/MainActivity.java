467ebda6b8dcd5d0d2bb4f7f4cd56a9e614a4650
abel wang <xbinwang@qq.com>
2016-10-31 10:30:30 +0800

    状态栏修改

--- app/src/main/java/com/ishow/ischool/activity/MainActivity.java
+++ app/src/main/java/com/ishow/ischool/activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.ishow.ischool.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.commonlib.util.LogUtil;
 import com.ishow.ischool.R;
@@ -77,6 +82,18 @@ public class MainActivity extends BaseActivity4Crm implements android.widget.Rad
                     .hide(businessFragment)
                     .hide(meFragment)
                     .commitAllowingStateLoss();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
         }
     }
 

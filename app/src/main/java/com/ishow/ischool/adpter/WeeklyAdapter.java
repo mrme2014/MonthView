85b5454d15d9a6619a90f0ab4663b851e03fa261
songyubao <863597281@qq.com>
2016-12-01 09:31:12 +0800

    修正周报 数据调用

--- app/src/main/java/com/ishow/ischool/adpter/WeeklyAdapter.java
+++ app/src/main/java/com/ishow/ischool/adpter/WeeklyAdapter.java
@@ -94,21 +94,21 @@ public class WeeklyAdapter extends RecyclerView.Adapter<WeeklyAdapter.ViewHolder
         holder.weekly_title.setText(valueDatas.get(realPosition).get(0));
 
         holder.item1_key.setText(keyDatas.get(0));
-        holder.item1_value.setText(valueDatas.get(realPosition).get(0));
+        holder.item1_value.setText(valueDatas.get(realPosition).get(1));
 
         holder.item2_key.setText(keyDatas.get(1));
-        holder.item2_value.setText(valueDatas.get(realPosition).get(1));
+        holder.item2_value.setText(valueDatas.get(realPosition).get(2));
 
         holder.item_key_add.setText(keyDatas.get(2));
-        holder.item_value_add.setText(valueDatas.get(realPosition).get(2));
+        holder.item_value_add.setText(valueDatas.get(realPosition).get(3));
 
         holder.item3_key.setText(keyDatas.get(3));
-        holder.item3_value.setText(valueDatas.get(realPosition).get(3));
+        holder.item3_value.setText(valueDatas.get(realPosition).get(4));
 
         holder.item4_key.setText(keyDatas.get(4));
-        holder.item4_value.setText(valueDatas.get(realPosition).get(4));
+        holder.item4_value.setText(valueDatas.get(realPosition).get(5));
         holder.item5_key.setText(keyDatas.get(5));
-        holder.item5_value.setText(valueDatas.get(realPosition).get(5));
+        holder.item5_value.setText(valueDatas.get(realPosition).get(6));
 
         holder.checkBox.setChecked(mCheckedSparseArray.get(position));
 
@@ -164,6 +164,7 @@ public class WeeklyAdapter extends RecyclerView.Adapter<WeeklyAdapter.ViewHolder
         TextView item_key_add;
         @BindView(R.id.item_value_add)
         TextView item_value_add;
+
         public ViewHolder(final View itemView) {
             super(itemView);
             if (itemView == mHeaderView) return;
